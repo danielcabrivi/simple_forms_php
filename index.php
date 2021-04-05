e5342584785d09e71126076433ab33b2dbd99628
Daniel Viana <daniel.viana.12@hotmail.com>
2021-04-04 21:19:44 -0300

    Adição do File upload

--- index.php
+++ index.php
@@ -2,4 +2,22 @@
 
 ?>
 
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Formulários</title>
+</head>
+<body>
+<nav>
+    <ul>
+        <li><a href="texts.html">Texts</a></li>
+        <li><a href="checkbox.html">Checkbox</a></li>
+        <li><a href="hidden.html">Hidden</a></li>
+        <li><a href="radio.html">Radio</a></li>
+        <li><a href="select.html">Select</a></li>
+        <li><a href="file.html">File</a></li>
+    </ul>
+</nav>
+</body>
+</html>
